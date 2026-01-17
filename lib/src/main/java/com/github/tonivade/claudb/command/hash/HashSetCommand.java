58f0c3f5114be3e2c89d27920fa8d38c5dffcd7b
Pavel Hurynovich <8087963+hurynovich@users.noreply.github.com>
2026-01-17 23:35:36 +0100

    Improved HashSetCommand to receive multiple key/value pairs

--- lib/src/main/java/com/github/tonivade/claudb/command/hash/HashSetCommand.java
+++ lib/src/main/java/com/github/tonivade/claudb/command/hash/HashSetCommand.java
@@ -10,29 +10,39 @@ import static com.github.tonivade.claudb.data.DatabaseValue.hash;
 import static com.github.tonivade.resp.protocol.RedisToken.integer;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.github.tonivade.claudb.command.CommandException;
 import com.github.tonivade.claudb.command.DBCommand;
 import com.github.tonivade.claudb.command.annotation.ParamType;
 import com.github.tonivade.claudb.data.DataType;
 import com.github.tonivade.claudb.data.Database;
 import com.github.tonivade.claudb.data.DatabaseValue;
 import com.github.tonivade.resp.annotation.Command;
-import com.github.tonivade.resp.annotation.ParamLength;
 import com.github.tonivade.resp.command.Request;
 import com.github.tonivade.resp.protocol.RedisToken;
 import com.github.tonivade.resp.protocol.SafeString;
 
 @Command("hset")
-@ParamLength(3)
 @ParamType(DataType.HASH)
 public class HashSetCommand implements DBCommand {
 
   @Override
   public RedisToken execute(Database db, Request request) {
-    DatabaseValue value = hash(entry(request.getParam(1), request.getParam(2)));
+    if (request.getLength() < 3 || request.getLength() % 2 != 1) {
+      throw new CommandException("Wrong number of arguments for 'HSET' command");
+    }
 
-    DatabaseValue resultValue = db.merge(safeKey(request.getParam(0)), value,
+    Map<SafeString, SafeString> value = new LinkedHashMap<>();
+    Iterator<SafeString> params = request.getParams().iterator();
+    SafeString key = params.next();
+    do {
+      value.put(params.next(), params.next());
+    } while (params.hasNext());
+
+    DatabaseValue resultValue = db.merge(safeKey(key), hash(value),
         (oldValue, newValue) -> {
           Map<SafeString, SafeString> merge = new HashMap<>();
           merge.putAll(oldValue.getHash());
